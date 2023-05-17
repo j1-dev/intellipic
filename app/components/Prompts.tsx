571275bef0245629e80b68068ade22d74d30c9a0
j1-dev <aephyxen@gmail.com>
2023-05-17 23:32:34 +0200

    Updated prompts

--- app/components/Prompts.tsx
+++ app/components/Prompts.tsx
@@ -6,37 +6,38 @@ export default function Prompts(){
   return(
     <div className="w-2/4 items-center ">
       <div className="Vikingo">
-      <a href="#" className="block p-9 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Instrucciones para crear un Avatar Vikingo</h5>
-      <div className="float-right">
-          <Image src="https://photoshot.app/prompts/sacha/viking.png" alt="" width={200} height={200} className="object-cover rounded-full" />
-        </div>
-      <h6 className="mb-2 text-xl tracking-tight text-gray-900 dark:text-white">Conviértete en un vikingo con nuestra guía de IA gratuita</h6>
-      <p className="font-normal text-gray-700 dark:text-gray-400">Retrato en primer plano de @Yo como un vikingo, ultra realista, arte conceptual, detalles intrincados, poderoso y feroz, altamente detallado, hiperrealista, renderizado con Octane, 8K, motor Unreal. Arte de Artgerm, Greg Rutkowski, Charlie Bowater, Magali Villeneuve y Alphonse Mucha, con la hora dorada, cuernos y trenzas en el pelo, capa y casco forrados de piel, hacha en mano, mirando hacia la cámara</p>
-      </a>
+        <a href="#" className="block p-9 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Instrucciones para crear un Avatar Vikingo</h5>
+          <div className="float-right">
+            <Image src="https://photoshot.app/prompts/sacha/viking.png" alt="" width={200} height={200} className="object-cover rounded-full" />
+          </div>
+          <h6 className="mb-2 text-xl tracking-tight text-gray-900 dark:text-white">Conviértete en un vikingo con nuestra guía de IA gratuita</h6>
+          <p className="font-normal text-gray-700 dark:text-gray-400">Retrato en primer plano de @Yo como un vikingo, ultra realista, arte conceptual, detalles intrincados, poderoso y feroz, altamente detallado, hiperrealista, renderizado con Octane, 8K, motor Unreal. Arte de Artgerm, Greg Rutkowski, Charlie Bowater, Magali Villeneuve y Alphonse Mucha, con la hora dorada, cuernos y trenzas en el pelo, capa y casco forrados de piel, hacha en mano, mirando hacia la cámara</p>
+        </a>
       </div>
       <div className="Vikingo">
-      <a href="#" className="block p-9 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Instrucciones para crear un Avatar Vikingo</h5>
-      <div className="float-right">
-          <Image src="https://photoshot.app/prompts/sacha/viking.png" alt="" width={200} height={200} className="object-cover rounded-full" />
-        </div>
-      <h6 className="mb-2 text-xl tracking-tight text-gray-900 dark:text-white">Conviértete en un vikingo con nuestra guía de IA gratuita</h6>
-      <p className="font-normal text-gray-700 dark:text-gray-400">Retrato en primer plano de @Yo como un vikingo, ultra realista, arte conceptual, detalles intrincados, poderoso y feroz, altamente detallado, hiperrealista, renderizado con Octane, 8K, motor Unreal. Arte de Artgerm, Greg Rutkowski, Charlie Bowater, Magali Villeneuve y Alphonse Mucha, con la hora dorada, cuernos y trenzas en el pelo, capa y casco forrados de piel, hacha en mano, mirando hacia la cámara</p>
-      </a>
+        <a href="#" className="block p-9 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Instrucciones para crear un Avatar Vikingo</h5>
+          <div className="float-right">
+            <Image src="https://photoshot.app/prompts/sacha/viking.png" alt="" width={200} height={200} className="object-cover rounded-full" />
+          </div>
+          <h6 className="mb-2 text-xl tracking-tight text-gray-900 dark:text-white">Conviértete en un vikingo con nuestra guía de IA gratuita</h6>
+          <p className="font-normal text-gray-700 dark:text-gray-400">Retrato en primer plano de @Yo como un vikingo, ultra realista, arte conceptual, detalles intrincados, poderoso y feroz, altamente detallado, hiperrealista, renderizado con Octane, 8K, motor Unreal. Arte de Artgerm, Greg Rutkowski, Charlie Bowater, Magali Villeneuve y Alphonse Mucha, con la hora dorada, cuernos y trenzas en el pelo, capa y casco forrados de piel, hacha en mano, mirando hacia la cámara</p>
+        </a>
       </div>
       <div className="Vikingo">
-      <a href="#" className="block p-9 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Instrucciones para crear un Avatar Vikingo</h5>
-      <div className="float-right">
-          <Image src="https://photoshot.app/prompts/sacha/viking.png" alt="" width={200} height={200} className="object-cover rounded-full" />
-        </div>
-      <h6 className="mb-2 text-xl tracking-tight text-gray-900 dark:text-white">Conviértete en un vikingo con nuestra guía de IA gratuita</h6>
-      <p className="font-normal text-gray-700 dark:text-gray-400">Retrato en primer plano de @Yo como un vikingo, ultra realista, arte conceptual, detalles intrincados, poderoso y feroz, altamente detallado, hiperrealista, renderizado con Octane, 8K, motor Unreal. Arte de Artgerm, Greg Rutkowski, Charlie Bowater, Magali Villeneuve y Alphonse Mucha, con la hora dorada, cuernos y trenzas en el pelo, capa y casco forrados de piel, hacha en mano, mirando hacia la cámara</p>
-      </a>
+        <a href="#" className="block p-9 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Instrucciones para crear un Avatar Vikingo</h5>
+          <div className="float-right">
+            <Image src="https://photoshot.app/prompts/sacha/viking.png" alt="" width={200} height={200} className="object-cover rounded-full" />
+          </div>
+          <h6 className="mb-2 text-xl tracking-tight text-gray-900 dark:text-white">Conviértete en un vikingo con nuestra guía de IA gratuita</h6>
+          <p className="font-normal text-gray-700 dark:text-gray-400">Retrato en primer plano de @Yo como un vikingo, ultra realista, arte conceptual, detalles intrincados, poderoso y feroz, altamente detallado, hiperrealista, renderizado con Octane, 8K, motor Unreal. Arte de Artgerm, Greg Rutkowski, Charlie Bowater, Magali Villeneuve y Alphonse Mucha, con la hora dorada, cuernos y trenzas en el pelo, capa y casco forrados de piel, hacha en mano, mirando hacia la cámara</p>
+        </a>
       </div>
-      </div>
-    )
-  }
+    </div>
+    
+  )
+}
 
   
