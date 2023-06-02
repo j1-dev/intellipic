1e41521582689f53db7eb77b93ede5d4e244a3af
j1-dev <aephyxen@gmail.com>
2023-06-02 13:21:44 +0200

    Import cleanup

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,5 @@
 'use client';
 import { useState, useEffect } from 'react';
-import { Auth } from '@supabase/auth-ui-react';
-import { ThemeSupa } from '@supabase/auth-ui-shared';
 import { supabase } from '../supabaseClient';
 import { useRouter } from 'next/navigation';
 import SignUp from '../components/SignUp';
