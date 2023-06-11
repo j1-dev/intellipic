2371e5159fdcb59868ecb93e7a40386c8f269876
David Sedeno <davidsed9@gmail.com>
2023-06-11 16:04:43 +0100

    Examples page updated (still needs work)

--- app/components/Prompts.tsx
+++ app/components/Prompts.tsx
@@ -24,7 +24,7 @@ export default function Prompts() {
             Conviértete en un vikingo con nuestra guía de IA gratuita
           </h6>
           <p className="font-normal text-gray-700 dark:text-gray-400">
-            Retrato en primer plano de @Yo como un vikingo, ultra realista, arte
+            Retrato en primer plano de @mi como un vikingo, ultra realista, arte
             conceptual, detalles intrincados, poderoso y feroz, altamente
             detallado, hiperrealista, renderizado con Octane, 8K, motor Unreal.
             Arte de Artgerm, Greg Rutkowski, Charlie Bowater, Magali Villeneuve
@@ -34,17 +34,17 @@ export default function Prompts() {
           </p>
         </a>
       </div>
-      <div className="Vikingo">
+      <div className="Astronauta">
         <a
           href="#"
           className="block p-9 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
         >
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Instrucciones para crear un Avatar Vikingo
+            Instrucciones para crear un Astronauta
           </h5>
           <div className="float-right">
             <Image
-              src="https://photoshot.app/prompts/sacha/viking.png"
+              src="https://photoshot.app/prompts/sacha/astronaut.png"
               alt=""
               width={200}
               height={200}
@@ -52,30 +52,29 @@ export default function Prompts() {
             />
           </div>
           <h6 className="mb-2 text-xl tracking-tight text-gray-900 dark:text-white">
-            Conviértete en un vikingo con nuestra guía de IA gratuita
+            Conviértete en un astronauta con nuestra guía de IA gratuita
           </h6>
           <p className="font-normal text-gray-700 dark:text-gray-400">
-            Retrato en primer plano de @Yo como un vikingo, ultra realista, arte
-            conceptual, detalles intrincados, poderoso y feroz, altamente
-            detallado, hiperrealista, renderizado con Octane, 8K, motor Unreal.
-            Arte de Artgerm, Greg Rutkowski, Charlie Bowater, Magali Villeneuve
-            y Alphonse Mucha, con la hora dorada, cuernos y trenzas en el pelo,
-            capa y casco forrados de piel, hacha en mano, mirando hacia la
-            cámara
+          Retrato detallado de cerca de @mi como astronauta,
+          futurista, altamente detallado, ultra realista,
+          arte conceptual, texturas intrincadas,
+          fondo interestelar, viaje espacial, 
+          arte por Alphonse Mucha, Ryan Kittleson,
+          Greg Rutkowski, Leesha Hannigan, Stephan Martiniere, Stanley "Artgerm" Lau.
           </p>
         </a>
       </div>
-      <div className="Vikingo">
+      <div className="Mago">
         <a
           href="#"
           className="block p-9 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
         >
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Instrucciones para crear un Avatar Vikingo
+            Instrucciones para crear un Mago
           </h5>
           <div className="float-right">
             <Image
-              src="https://photoshot.app/prompts/sacha/viking.png"
+              src="https://photoshot.app/prompts/romy/wizard.png"
               alt=""
               width={200}
               height={200}
@@ -83,16 +82,147 @@ export default function Prompts() {
             />
           </div>
           <h6 className="mb-2 text-xl tracking-tight text-gray-900 dark:text-white">
-            Conviértete en un vikingo con nuestra guía de IA gratuita
+            Conviértete en Mago con nuestra sugerencia gratuita de IA.
           </h6>
           <p className="font-normal text-gray-700 dark:text-gray-400">
-            Retrato en primer plano de @Yo como un vikingo, ultra realista, arte
-            conceptual, detalles intrincados, poderoso y feroz, altamente
-            detallado, hiperrealista, renderizado con Octane, 8K, motor Unreal.
-            Arte de Artgerm, Greg Rutkowski, Charlie Bowater, Magali Villeneuve
-            y Alphonse Mucha, con la hora dorada, cuernos y trenzas en el pelo,
-            capa y casco forrados de piel, hacha en mano, mirando hacia la
-            cámara
+          Retrato detallado de cerca de @mi como un mago, con un concepto de arte fantástico, 
+          con detalles e texturas intrincadas, mágico, colorido, arte por wlop, greg rutkowski,
+          charlie bowater, magali villeneuve, alphonse mucha, surrealista, @me mirando hacia el horizonte,
+          sosteniendo un bastón, con fuego y estrellas en el fondo.
+          </p>
+        </a>
+      </div>
+      <div className="Hobbit">
+        <a
+          href="#"
+          className="block p-9 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        >
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Instrucciones para crear un Hobbit
+          </h5>
+          <div className="float-right">
+            <Image
+              src="https://photoshot.app/prompts/romy/hobbit.png"
+              alt=""
+              width={200}
+              height={200}
+              className="object-cover rounded-full"
+            />
+          </div>
+          <h6 className="mb-2 text-xl tracking-tight text-gray-900 dark:text-white">
+            Conviértete en Hobbit con nuestra sugerencia gratuita de IA.
+          </h6>
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+          Retrato detallado de cerca de @mi como un Hobbit, pequeño, con grandes ojos marrones,
+           ropa verde y marrón, rasgos faciales detallados, pies pequeños, cabello ondulado,
+            concepto de arte fantástico, tendencia en ArtStation, altamente detallado, arte de John Howe,
+             Alan Lee y Weta Workshop, colores terrosos, mirando a la cámara.
+          </p>
+        </a>
+      </div>
+      <div className="Payaso">
+        <a
+          href="#"
+          className="block p-9 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        >
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Instrucciones para crear un Payaso
+          </h5>
+          <div className="float-right">
+            <Image
+              src="https://photoshot.app/prompts/sacha/clown.png"
+              alt=""
+              width={200}
+              height={200}
+              className="object-cover rounded-full"
+            />
+          </div>
+          <h6 className="mb-2 text-xl tracking-tight text-gray-900 dark:text-white">
+            Conviértete en Payaso con nuestra sugerencia gratuita de IA.
+          </h6>
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+          Retrato detallado de cerca de @mi como un payaso, altamente detallado, 
+          surrealista, rostro inexpresivo, colores brillantes, iluminación contrastante, 
+          fondo abstracto, arte de wlop, Greg Rutkowski, Charlie Bowater, Magali Villeneuve,
+           Alphonse Mucha, estilo caricaturesco, inspirado en cómics.
+          </p>
+        </a>
+      </div>
+      <div className="Mago">
+        <a
+          href="#"
+          className="block p-9 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        >
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Instrucciones para crear un Hobbit
+          </h5>
+          <div className="float-right">
+            <Image
+              src="https://photoshot.app/prompts/romy/hobbit.png"
+              alt=""
+              width={200}
+              height={200}
+              className="object-cover rounded-full"
+            />
+          </div>
+          <h6 className="mb-2 text-xl tracking-tight text-gray-900 dark:text-white">
+            Conviértete en Hobbit con nuestra sugerencia gratuita de IA.
+          </h6>
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+          Retrato detallado de cerca de @Yo como un Hobbit, pequeño, con grandes ojos marrones, ropa verde y marrón, rasgos faciales detallados, pies pequeños, cabello ondulado, concepto de arte fantástico, tendencia en ArtStation, altamente detallado, arte de John Howe, Alan Lee y Weta Workshop, colores terrosos, mirando a la cámara.
+          </p>
+        </a>
+      </div>
+      <div className="Mago">
+        <a
+          href="#"
+          className="block p-9 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        >
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Instrucciones para crear un Mago
+          </h5>
+          <div className="float-right">
+            <Image
+              src="https://photoshot.app/prompts/romy/wizard.png"
+              alt=""
+              width={200}
+              height={200}
+              className="object-cover rounded-full"
+            />
+          </div>
+          <h6 className="mb-2 text-xl tracking-tight text-gray-900 dark:text-white">
+            Conviértete en Mago con nuestra sugerencia gratuita de IA.
+          </h6>
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+          Retrato detallado de cerca de @mi como un mago, con un concepto de arte fantástico, 
+          con detalles e texturas intrincadas, mágico, colorido, arte por wlop, greg rutkowski,
+          charlie bowater, magali villeneuve, alphonse mucha, surrealista, @me mirando hacia el horizonte,
+          sosteniendo un bastón, con fuego y estrellas en el fondo.
+          </p>
+        </a>
+      </div>
+      <div className="Mago">
+        <a
+          href="#"
+          className="block p-9 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        >
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Instrucciones para crear un Hobbit
+          </h5>
+          <div className="float-right">
+            <Image
+              src="https://photoshot.app/prompts/romy/hobbit.png"
+              alt=""
+              width={200}
+              height={200}
+              className="object-cover rounded-full"
+            />
+          </div>
+          <h6 className="mb-2 text-xl tracking-tight text-gray-900 dark:text-white">
+            Conviértete en Hobbit con nuestra sugerencia gratuita de IA.
+          </h6>
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+          Retrato detallado de cerca de @mi como un Hobbit, pequeño, con grandes ojos marrones, ropa verde y marrón, rasgos faciales detallados, pies pequeños, cabello ondulado, concepto de arte fantástico, tendencia en ArtStation, altamente detallado, arte de John Howe, Alan Lee y Weta Workshop, colores terrosos, mirando a la cámara.
           </p>
         </a>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -12,11 +12,11 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <Link href="/login">login</Link>
+        <Link href="/login">Login</Link>
         <Morph texts={['Intellipic', 'Remake', 'Yourself']} />
-        <Link href="/register">register</Link>
+        <Link href="/register">Register</Link>
       </div>
-      <button onClick={handleLogout}>logout</button>
+      <button onClick={handleLogout}>Logout</button>
     </main>
   );
 }
