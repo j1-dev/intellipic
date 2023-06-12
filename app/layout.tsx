519367ccca0483d092470ec14364478800379c6b
j1-dev <aephyxen@gmail.com>
2023-06-12 13:39:27 +0200

    Implemented next/font optimization for faster rendering

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,50 @@ import './globals.css';
 import SupabaseProvider from './supabase-provider';
 import ThemeSwitcher from './components/ThemeSwitcher';
 import { Toaster } from 'react-hot-toast';
+import localFont from 'next/font/local';
 
 export const metadata = {
   title: 'IntelliPic',
   description: 'IntelliPic app to make custom AI Portrait models'
 };
+
+const font = localFont({
+  // src: './core/fonts/SFProDisplay-Regular.ttf',
+  src: [
+    {
+      path: './core/fonts/SFProDisplay-Regular.ttf',
+      weight: '400',
+      style: 'normal'
+    },
+    {
+      path: './core/fonts/SFProDisplay-Medium.ttf',
+      weight: '500',
+      style: 'normal'
+    },
+    {
+      path: './core/fonts/SFProDisplay-Semibold.ttf',
+      weight: '600',
+      style: 'normal'
+    },
+    {
+      path: './core/fonts/SFProDisplay-Bold.ttf',
+      weight: '700',
+      style: 'normal'
+    },
+    {
+      path: './core/fonts/SFProDisplay-Heavy.ttf',
+      weight: '800',
+      style: 'normal'
+    },
+    {
+      path: './core/fonts/SFProDisplay-Black.ttf',
+      weight: '900',
+      style: 'normal'
+    }
+  ],
+  display: 'swap',
+  variable: '--font-sans'
+});
 
 export default function RootLayout({
   children
@@ -14,7 +53,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={font.className}>
       <body>
         <SupabaseProvider>
           <Toaster position="bottom-right" reverseOrder={false} />
