339fb1e5a9eb015648adb39fc7936a6c269e0b7c
j1-dev <aephyxen@gmail.com>
2023-06-22 17:51:51 +0200

    Fixed navbar for mobile

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -16,42 +16,42 @@ export default function Navbar() {
   }, []);
 
   return (
-    <div className="Navbar font-sans text-center bg-white dark:bg-black border-b-[1px] border-black dark:border-white shadow-sm transition-all">
+    <div className="font-sans text-center bg-white dark:bg-black border-b-[1px] border-black dark:border-white shadow-sm transition-all">
       <ul className="flex justify-center items-center h-16">
-        <li className="list-none mx-2 my-1 relative group" key={Math.random()}>
+        <li className="list-none mx-2 my-2 relative group" key={Math.random()}>
           <Link
             href={`/dashboard/${user?.id}`}
-            className="py-2 px-6 text-black dark:text-white no-underline text-xl relative group-hover:text-gray-500 transition-all duration-350 ease-in-out"
+            className="hover:font-bold lg:text-2xl md:text-xl xs:text-lg py-2 px-6 text-black dark:text-white no-underline text-xl relative group-hover:text-gray-500 transition-all duration-350 ease-in-out"
           >
             Modelos
             <span className="absolute top-0 right-0 h-0 w-0 border-t border-r border-black dark:border-white opacity-0 group-hover:opacity-100 group-hover:h-[14px] group-hover:w-[14px] transition-all duration-350 ease-in-out transform -translate-x-full -translate-y-1/2"></span>
             <span className="absolute bottom-0 left-0 h-0 w-0 border-b border-l border-black dark:border-white opacity-0 group-hover:opacity-100 group-hover:h-[14px] group-hover:w-[14px] transition-all duration-350 ease-in-out transform translate-x-full translate-y-1/2"></span>
           </Link>
         </li>
-        <li className="list-none mx-2 my-1 relative group" key={Math.random()}>
+        <li className="list-none mx-2 my-2 relative group" key={Math.random()}>
           <Link
             href={`/dashboard/${user?.id}/train`}
-            className="py-2 px-6 text-black dark:text-white no-underline text-xl relative group-hover:text-gray-500 transition-all duration-350 ease-in-out"
+            className="hover:font-bold lg:text-2xl md:text-xl xs:text-lg py-2 px-6 text-black dark:text-white no-underline text-xl relative group-hover:text-gray-500 transition-all duration-350 ease-in-out"
           >
             Entrenar
             <span className="absolute top-0 right-0 h-0 w-0 border-t border-r border-black dark:border-white opacity-0 group-hover:opacity-100 group-hover:h-[14px] group-hover:w-[14px] transition-all duration-350 ease-in-out transform -translate-x-full -translate-y-1/2"></span>
             <span className="absolute bottom-0 left-0 h-0 w-0 border-b border-l border-black dark:border-white opacity-0 group-hover:opacity-100 group-hover:h-[14px] group-hover:w-[14px] transition-all duration-350 ease-in-out transform translate-x-full translate-y-1/2"></span>
           </Link>
         </li>
-        <li className="list-none mx-2 my-1 relative group" key={Math.random()}>
+        <li className="list-none mx-2 my-2 relative group" key={Math.random()}>
           <Link
             href="/dashboard/examples"
-            className="py-2 px-6 text-black dark:text-white no-underline text-xl relative group-hover:text-gray-500 transition-all duration-350 ease-in-out"
+            className="hover:font-bold lg:text-2xl md:text-xl xs:text-lg py-2 px-6 text-black dark:text-white no-underline text-xl relative group-hover:text-gray-500 transition-all duration-350 ease-in-out"
           >
             Ejemplos
             <span className="absolute top-0 right-0 h-0 w-0 border-t border-r border-black dark:border-white opacity-0 group-hover:opacity-100 group-hover:h-[14px] group-hover:w-[14px] transition-all duration-350 ease-in-out transform -translate-x-full -translate-y-1/2"></span>
             <span className="absolute bottom-0 left-0 h-0 w-0 border-b border-l border-black dark:border-white opacity-0 group-hover:opacity-100 group-hover:h-[14px] group-hover:w-[14px] transition-all duration-350 ease-in-out transform translate-x-full translate-y-1/2"></span>
           </Link>
         </li>
-        <li className="list-none mx-2 my-1 relative group" key={Math.random()}>
+        <li className="list-none mx-2 my-2 relative group" key={Math.random()}>
           <Link
             href="/dashboard/shop"
-            className="py-2 px-6 text-black dark:text-white no-underline text-xl relative group-hover:text-gray-500 transition-all duration-350 ease-in-out"
+            className="hover:font-bold lg:text-2xl md:text-xl xs:text-lg py-2 px-6 text-black dark:text-white no-underline text-xl relative group-hover:text-gray-500 transition-all duration-350 ease-in-out"
           >
             Tienda
             <span className="absolute top-0 right-0 h-0 w-0 border-t border-r border-black dark:border-white opacity-0 group-hover:opacity-100 group-hover:h-[14px] group-hover:w-[14px] transition-all duration-350 ease-in-out transform -translate-x-full -translate-y-1/2"></span>
