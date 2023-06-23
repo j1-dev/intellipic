5d4be10b11c276f5476aa9560adab333eb382275
j1-dev <aephyxen@gmail.com>
2023-06-23 17:39:43 +0200

    Changes for more cohesive styling

--- app/dashboard/[userId]/page.tsx
+++ app/dashboard/[userId]/page.tsx
@@ -28,24 +28,28 @@ export default function DashboardPage() {
   }, []);
 
   return (
-    <div>
-      {models ? (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {models.map((data: any) => {
-            const props = {
-              userId: data.user_id as string,
-              modelId: data.run_id as string,
-              token: data.prompt_token as string,
-              status: data.status as string
-            };
-            return <ModelCard props={props} key={data.run_id} />;
-          })}
-        </div>
-      ) : (
-        <div className="w-40 m-auto mt-24">
-          <PulseLoader color="#B6B6B6" />
-        </div>
-      )}
+    <div className="py-8">
+      <div className="max-w-screen-lg mx-auto px-8">
+        <h2 className="text-2xl font-bold mb-4">Modelos 🤖</h2>
+
+        {models ? (
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {models.map((data: any) => {
+              const props = {
+                userId: data.user_id as string,
+                modelId: data.run_id as string,
+                token: data.prompt_token as string,
+                status: data.status as string
+              };
+              return <ModelCard props={props} key={data.run_id} />;
+            })}
+          </div>
+        ) : (
+          <div className="w-40 m-auto mt-24">
+            <PulseLoader color="#B6B6B6" />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
