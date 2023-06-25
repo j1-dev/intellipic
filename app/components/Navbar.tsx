94df0ec999dafb03a9cc3d5bb393808fec1746db
j1-dev <aephyxen@gmail.com>
2023-06-25 18:37:25 +0200

    Fixed log out button position to be inside the Navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -24,17 +24,17 @@ export default function Navbar() {
   };
 
   return (
-    <div className="font-sans text-center bg-white dark:bg-black border-b-[1px] border-black dark:border-white shadow-sm transition-all ">
-      <ul className="flex justify-end items-center h-20 px-8">
+    <div className="relative font-sans text-center bg-white dark:bg-black border-b-[1px] border-black dark:border-white shadow-sm transition-all ">
+      {/* <ul className="flex justify-end items-center h-20 px-8">
         <li className="list-none">
           <button
             onClick={handleLogout}
-            className="bg-white text-black border-black hover:bg-black hover:text-white dark:bg-black dark:text-white dark:border-white dark:hover:bg-white dark:hover:text-black border rounded py-2 px-4 transition-all font-bold"
+            className="absolute top-10 right-10 bg-white text-black border-black hover:bg-black hover:text-white dark:bg-black dark:text-white dark:border-white dark:hover:bg-white dark:hover:text-black border rounded py-2 px-4 transition-all font-bold"
           >
             Log Out
           </button>
         </li>
-      </ul>
+      </ul> */}
       <ul className="flex justify-center items-center h-20">
         <li
           className="list-none lg:mx-2 mx-0  my-2 relative group"
@@ -42,7 +42,7 @@ export default function Navbar() {
         >
           <Link
             href={`/dashboard/${user?.id}`}
-            className="font-bold lg:text-2xl md:text-xl xs:text-lg py-2 lg:px-7 px-4 text-black dark:text-white no-underline text-xl relative transition-all duration-350 ease-in-out"
+            className="font-bold lg:text-2xl md:text-xl xs:text-base py-2 lg:px-7 px-4 text-black dark:text-white no-underline text-xl relative transition-all duration-350 ease-in-out"
           >
             Modelos
             <span className="absolute top-0 right-0 h-0 w-0 border-t border-r border-black dark:border-white opacity-0 group-hover:opacity-100 group-hover:h-[14px] group-hover:w-[14px] transition-all duration-350 ease-in-out transform -translate-x-full -translate-y-1/2"></span>
@@ -55,7 +55,7 @@ export default function Navbar() {
         >
           <Link
             href={`/dashboard/${user?.id}/train`}
-            className="font-bold lg:text-2xl md:text-xl xs:text-lg py-2 lg:px-7 px-4 text-black dark:text-white no-underline text-xl relative transition-all duration-350 ease-in-out"
+            className="font-bold lg:text-2xl md:text-xl xs:text-base py-2 lg:px-7 px-4 text-black dark:text-white no-underline text-xl relative transition-all duration-350 ease-in-out"
           >
             Entrenar
             <span className="absolute top-0 right-0 h-0 w-0 border-t border-r border-black dark:border-white opacity-0 group-hover:opacity-100 group-hover:h-[14px] group-hover:w-[14px] transition-all duration-350 ease-in-out transform -translate-x-full -translate-y-1/2"></span>
@@ -68,7 +68,7 @@ export default function Navbar() {
         >
           <Link
             href="/dashboard/examples"
-            className="font-bold lg:text-2xl md:text-xl xs:text-lg py-2 lg:px-7 px-4 text-black dark:text-white no-underline text-xl relative transition-all duration-350 ease-in-out"
+            className="font-bold lg:text-2xl md:text-xl xs:text-base py-2 lg:px-7 px-4 text-black dark:text-white no-underline text-xl relative transition-all duration-350 ease-in-out"
           >
             Ejemplos
             <span className="absolute top-0 right-0 h-0 w-0 border-t border-r border-black dark:border-white opacity-0 group-hover:opacity-100 group-hover:h-[14px] group-hover:w-[14px] transition-all duration-350 ease-in-out transform -translate-x-full -translate-y-1/2"></span>
@@ -81,12 +81,23 @@ export default function Navbar() {
         >
           <Link
             href="/dashboard/shop"
-            className="font-bold lg:text-2xl md:text-xl xs:text-lg py-2 lg:px-7 px-4 text-black dark:text-white no-underline text-xl relative transition-all duration-350 ease-in-out"
+            className="font-bold lg:text-2xl md:text-xl xs:text-base py-2 lg:px-7 px-4 text-black dark:text-white no-underline text-xl relative transition-all duration-350 ease-in-out"
           >
             Tienda
             <span className="absolute top-0 right-0 h-0 w-0 border-t border-r border-black dark:border-white opacity-0 group-hover:opacity-100 group-hover:h-[14px] group-hover:w-[14px] transition-all duration-350 ease-in-out transform -translate-x-full -translate-y-1/2"></span>
             <span className="absolute bottom-0 left-0 h-0 w-0 border-b border-l border-black dark:border-white opacity-0 group-hover:opacity-100 group-hover:h-[14px] group-hover:w-[14px] transition-all duration-350 ease-in-out transform translate-x-full translate-y-1/2"></span>
           </Link>
+        </li>
+        <li
+          className="list-none lg:mx-2 mx-0 my-2 relative group"
+          key={Math.random()}
+        >
+          <button
+            onClick={handleLogout}
+            className=" bg-white text-black border-black hover:bg-black hover:text-white dark:bg-black dark:text-white dark:border-white dark:hover:bg-white dark:hover:text-black border rounded py-2 px-4 transition-all"
+          >
+            Log Out
+          </button>
         </li>
       </ul>
     </div>
