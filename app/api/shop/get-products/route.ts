10d54b6ff43a728ee9700e7cc9dc41aed0bac713
j1-dev <aephyxen@gmail.com>
2023-06-28 13:43:22 +0200

    Api changes to work with current Product interface

--- app/api/shop/get-products/route.ts
+++ app/api/shop/get-products/route.ts
@@ -1,12 +1,34 @@
 import Stripe from 'stripe';
 import { NextResponse } from 'next/server';
 
+interface Product {
+  id: string;
+  name: string;
+  price: number;
+  features: string[];
+}
+
 export async function GET(request: any) {
   const config: Stripe.StripeConfig = { apiVersion: '2022-11-15' };
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, config);
   const prices = await stripe.prices.list({
     limit: 5
   });
+  const products = await stripe.products.list({
+    limit: 5
+  });
+  const resArray: Product[] = new Array<Product>();
+  prices.data.map((price, index) => {
+    const pric = prices.data[index];
+    const prod = products.data[index];
+    const producto: Product = {
+      id: prod.id,
+      name: prod.name,
+      price: pric.unit_amount as number,
+      features: prod.attributes as string[]
+    };
+    resArray.push(producto);
+  });
 
-  return NextResponse.json(prices.data.reverse());
+  return NextResponse.json(resArray.reverse());
 }
