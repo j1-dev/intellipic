f3c6022ca981ae3344fb5b39678da6e7407adc31
j1-dev <aephyxen@gmail.com>
2023-07-02 19:40:13 +0200

    Added functionality to the checkout api route

--- app/api/shop/checkout/route.ts
+++ app/api/shop/checkout/route.ts
@@ -1,9 +1,14 @@
-import { NextResponse, NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
 import stripe from '../../../core/clients/stripe';
+import { supabase } from '@/app/supabaseClient';
 
 export async function POST(request: any) {
   let data = await request.json();
   let priceId = data.priceId;
+  let userId = data.userId;
+  let tokenAmountGenerating = 0;
+  let tokenAmountTraining = 0;
+
   const session = await stripe.checkout.sessions.create({
     line_items: [
       {
@@ -12,9 +17,50 @@ export async function POST(request: any) {
       }
     ],
     mode: 'payment',
-    success_url: 'http://localhost:3000',
-    cancel_url: 'http://localhost:3000'
+    success_url: 'http://localhost:3000/dashboard/shop/order-success',
+    cancel_url: 'http://localhost:3000/dashboard/shop/'
   });
+
+  switch (session.amount_total) {
+    case 100:
+      tokenAmountGenerating = 10;
+      break;
+    case 200:
+      tokenAmountGenerating = 30;
+      break;
+    case 300:
+      tokenAmountGenerating = 50;
+      break;
+    case 350:
+      tokenAmountGenerating = 20;
+      tokenAmountTraining = 1;
+      break;
+    case 500:
+      tokenAmountGenerating = 100;
+      break;
+    default:
+      break;
+  }
+
+  console.log(tokenAmountGenerating);
+  console.log(tokenAmountTraining);
+
+  console.log(session.amount_total);
+
+  await supabase.from('payments').insert({
+    id: session.id,
+    payment_status: session.payment_status,
+    token_amount_generating: tokenAmountGenerating,
+    token_amount_training: tokenAmountTraining
+  });
+  console.log(userId);
+  await supabase
+    .from('user-data')
+    .update({
+      last_payment_id: session.id,
+      last_payment_status: session.payment_status
+    })
+    .eq('id', userId);
 
   return NextResponse.json(session.url);
 }
