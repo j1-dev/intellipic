c8cd421a54d192bc0f880e692b4524ea0b22c51d
j1-dev <aephyxen@gmail.com>
2023-07-24 12:25:09 +0200

    Tweaked home navbar logo

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,12 @@ import Carousel from './components/Carousel';
 import Image from 'next/image';
 import Logo from './core/resources/logo';
 
-
 const slides = [
-  "/Images/3l0n.png",
-  "/Images/out-0 (7).png",
-  "https://i.ibb.co/XXR8kzF/3.png",
-  "https://i.ibb.co/yg7BSdM/4.png",
-]
+  '/Images/3l0n.png',
+  '/Images/out-0 (7).png',
+  'https://i.ibb.co/XXR8kzF/3.png',
+  'https://i.ibb.co/yg7BSdM/4.png'
+];
 
 /**
  * @TODO: Document everything
@@ -26,7 +25,9 @@ export default async function Home() {
       <header className="py-8">
         <nav className="max-w-screen-lg mx-auto flex justify-between items-center px-8">
           <Logo />
-          <h1 className=" absolute left-80 text-3xl font-bold hidden xs:block">IntelliPic</h1>
+          <h1 className="text-2xl sm:text-3xl font-bold hidden xs:block m-0">
+            IntelliPic
+          </h1>
           <button className=" bg-white text-black border-black hover:bg-black hover:text-white dark:bg-black dark:text-white dark:border-white dark:hover:bg-white dark:hover:text-black border rounded py-2 px-3 transition-all ml-0 xs:ml-auto float-left xs:float-none">
             <Link href="/login">Inicia sesión</Link>
           </button>
@@ -41,7 +42,9 @@ export default async function Home() {
           <Morph texts={['IntelliPic', ' Remake', 'Yourself']} />
           <div className="text-center">
             <p className="text-lg mb-8">
-            Desata tu creatividad con Intellipic. Sube cualquier imagen, entrena una red neuronal y transfórmala en una increíble obra de arte con varios estilos. ✨🎨
+              Desata tu creatividad con Intellipic. Sube cualquier imagen,
+              entrena una red neuronal y transfórmala en una increíble obra de
+              arte con varios estilos. ✨🎨
             </p>
             <button className=" bg-white text-black border-black hover:bg-black hover:text-white dark:bg-black dark:text-white dark:border-white dark:hover:bg-white dark:hover:text-black border rounded py-2 px-4 transition-all">
               <Link href="/register">Comenzar</Link>
@@ -57,39 +60,58 @@ export default async function Home() {
             <div className="border feature-card bg-white dark:bg-black dark:border-white border-black p-6 rounded-lg transition-all duration-300 transform hover:scale-105">
               <h3 className="text-xl font-bold mb-2">Subir imagen 📷</h3>
               <p>
-              Sube sin problemas tus imágenes favoritas de cualquier tema que desees.
+                Sube sin problemas tus imágenes favoritas de cualquier tema que
+                desees.
               </p>
             </div>
             <div className="border feature-card bg-white dark:bg-black dark:border-white border-black p-6 rounded-lg transition-all duration-300 transform hover:scale-105">
               <h3 className="text-xl font-bold mb-2">
-              Entrenamiento de la red neuronal🧠
+                Entrenamiento de la red neuronal🧠
               </h3>
               <p>
-              Utiliza tecnología de vanguardia de redes neuronales para aprender de las imágenes subidas y comprender las características únicas del sujeto.
+                Utiliza tecnología de vanguardia de redes neuronales para
+                aprender de las imágenes subidas y comprender las
+                características únicas del sujeto.
               </p>
             </div>
             <div className="border feature-card bg-white dark:bg-black dark:border-white border-black p-6 rounded-lg transition-all duration-300 transform hover:scale-105">
               <h3 className="text-xl font-bold mb-2">Selección de estilo 🌈</h3>
               <p>
-              Elige entre una amplia colección de estilos para aplicar a tus imágenes, lo que te permitirá crear combinaciones visuales impresionantes.
+                Elige entre una amplia colección de estilos para aplicar a tus
+                imágenes, lo que te permitirá crear combinaciones visuales
+                impresionantes.
               </p>
             </div>
             <div className="border feature-card bg-white dark:bg-black dark:border-white border-black p-6 rounded-lg transition-all duration-300 transform hover:scale-105">
-              <h3 className="text-xl font-bold mb-2">Generación de imágenes⚡️</h3>
+              <h3 className="text-xl font-bold mb-2">
+                Generación de imágenes⚡️
+              </h3>
               <p>
-              Observa con asombro cómo Intellipic genera nuevas imágenes impresionantes, fusionando el sujeto con el estilo elegido en tiempo real.
+                Observa con asombro cómo Intellipic genera nuevas imágenes
+                impresionantes, fusionando el sujeto con el estilo elegido en
+                tiempo real.
               </p>
             </div>
             <div className="border feature-card bg-white dark:bg-black dark:border-white border-black p-6 rounded-lg transition-all duration-300 transform hover:scale-105">
-              <h3 className="text-xl font-bold mb-2">Creatividad infinita 🔁</h3>
+              <h3 className="text-xl font-bold mb-2">
+                Creatividad infinita 🔁
+              </h3>
               <p>
-              Con Intellipic, no hay límites para tu imaginación. Sigue experimentando, entrenando y generando para descubrir resultados cautivadores.
+                Con Intellipic, no hay límites para tu imaginación. Sigue
+                experimentando, entrenando y generando para descubrir resultados
+                cautivadores.
               </p>
             </div>
             <div className="border feature-card bg-white dark:bg-black dark:border-white border-black p-6 rounded-lg transition-all duration-300 transform hover:scale-105">
-              <h3 className="text-xl font-bold mb-2">El trabajo impulsado por IA tiene costos asociados. 💡</h3>
+              <h3 className="text-xl font-bold mb-2">
+                El trabajo impulsado por IA tiene costos asociados. 💡
+              </h3>
               <p>
-              Desata el potencial de la generación de imágenes mediante IA. Debido a los recursos sustanciales necesarios para el entrenamiento, no es factible proporcionar este servicio de forma gratuita. Tu inversión impulsa la innovación y garantiza resultados de la más alta calidad.
+                Desata el potencial de la generación de imágenes mediante IA.
+                Debido a los recursos sustanciales necesarios para el
+                entrenamiento, no es factible proporcionar este servicio de
+                forma gratuita. Tu inversión impulsa la innovación y garantiza
+                resultados de la más alta calidad.
               </p>
             </div>
           </div>
@@ -100,10 +122,14 @@ export default async function Home() {
         <div className="max-w-screen-lg mx-auto px-8">
           <h2 className="text-2xl font-bold mb-4">Ejemplos📸</h2>
           <div className="carousel">
-            <Carousel autoSlide={true} autoSlideInterval={10000} slidesPerPage={1}>
-              {slides.map((s: string)  => (
-                <Image src={s} alt='slide' width="800" height="600" key={s} />
-               ))}
+            <Carousel
+              autoSlide={true}
+              autoSlideInterval={10000}
+              slidesPerPage={1}
+            >
+              {slides.map((s: string) => (
+                <Image src={s} alt="slide" width="800" height="600" key={s} />
+              ))}
             </Carousel>
           </div>
         </div>
@@ -157,11 +183,12 @@ export default async function Home() {
         <div className="max-w-screen-lg mx-auto px-8">
           <h2 className="text-2xl font-bold mb-4">Privacidad y Seguridad 🔒</h2>
           <p className="text-lg font-semibold mb-8">
-          En Intellipic, priorizamos tu privacidad y seguridad.
-           Para asegurarnos de que tus imágenes estén protegidas, 
-          eliminamos todas las imágenes subidas y las obras de arte generadas después de 24 horas.
-           Esto evita cualquier uso malicioso o acceso no autorizado a tus datos. Puedes utilizar Intellipic con confianza,
-          sabiendo que tu privacidad es nuestra máxima prioridad.
+            En Intellipic, priorizamos tu privacidad y seguridad. Para
+            asegurarnos de que tus imágenes estén protegidas, eliminamos todas
+            las imágenes subidas y las obras de arte generadas después de 24
+            horas. Esto evita cualquier uso malicioso o acceso no autorizado a
+            tus datos. Puedes utilizar Intellipic con confianza, sabiendo que tu
+            privacidad es nuestra máxima prioridad.
           </p>
         </div>
       </section>
@@ -201,7 +228,7 @@ export default async function Home() {
                 href="#"
                 className="text-gray-900 dark:text-white hover:text-black dark:hover:text-gray-400"
               >
-                <FaInstagram size={24} className='mx-3'/>
+                <FaInstagram size={24} className="mx-3" />
               </a>
             </li>
             <li>
@@ -209,7 +236,7 @@ export default async function Home() {
                 href="#"
                 className="text-gray-900 dark:text-white hover:text-black dark:hover:text-gray-400"
               >
-                <FaGithub size={24} className='mx-3'/>
+                <FaGithub size={24} className="mx-3" />
               </a>
             </li>
             <li>
@@ -217,7 +244,7 @@ export default async function Home() {
                 href="#"
                 className="text-gray-900 dark:text-white hover:text-black dark:hover:text-gray-400"
               >
-                <FaDiscord size={24} className='mx-3'/>
+                <FaDiscord size={24} className="mx-3" />
               </a>
             </li>
             <li>
@@ -225,7 +252,7 @@ export default async function Home() {
                 href="#"
                 className="text-gray-900 dark:text-white hover:text-black dark:hover:text-gray-400"
               >
-                <FaLinkedin size={24} className='mx-3'/>
+                <FaLinkedin size={24} className="mx-3" />
               </a>
             </li>
           </ul>
