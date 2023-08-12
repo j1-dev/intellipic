013e2760e4ded6d7480e51e6dcf42943307962e3
j1-dev <aephyxen@gmail.com>
2023-08-12 13:02:08 +0200

    Added custom options to createClient

--- app/core/clients/supabase.ts
+++ app/core/clients/supabase.ts
@@ -1,9 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseURL: string = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const supabaseAnonKey: string = process.env
-  .NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
-
-const supabase = createClient(supabaseURL, supabaseAnonKey);
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
+  {
+    auth: { persistSession: true }
+  }
+);
 
 export default supabase;
