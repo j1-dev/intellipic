0c6e150f5f63858cc2be3227eb602bc5760fea30
j1-dev <aephyxen@gmail.com>
2023-08-12 13:06:54 +0200

    Fixed small mistake

--- app/core/clients/supabase.ts
+++ app/core/clients/supabase.ts
@@ -4,7 +4,7 @@ const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL as string,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
   {
-    auth: { persistSession: true }
+    auth: { persistSession: false }
   }
 );
 
