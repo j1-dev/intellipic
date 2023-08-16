3e3170dc43ac440fa3d2f17d93b136781385d22c
j1-dev <aephyxen@gmail.com>
2023-08-16 10:29:45 +0200

    Minor change to supabase Client

--- app/core/clients/supabase.ts
+++ app/core/clients/supabase.ts
@@ -27,7 +27,8 @@ const supabase: SupabaseClient = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
   {
     auth: {
-      persistSession: false
+      persistSession: false,
+      autoRefreshToken: false
     }
   }
 );
