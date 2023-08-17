9dc0f917083af560b153baf06c276b89c898a89f
David Sedeno <davidsed9@gmail.com>
2023-08-17 16:11:44 +0100

    First Steps for changing examples page

--- app/components/PromptCard.tsx
+++ app/components/PromptCard.tsx
@@ -1,49 +1,17 @@
 import React from 'react';
 import Image from 'next/image';
-import toast from 'react-hot-toast';
 
-function PromptCard({
-  props
-}: {
-  props: { img: string; text1: string; text2: string; text3: string };
-}) {
-  function handleCopy(text: string) {
-    return function (event: React.MouseEvent<HTMLAnchorElement>) {
-      event.preventDefault(); // Prevent default navigation behavior
+interface PromptCardProps {
+  img: string;
+}
 
-      navigator.clipboard.writeText(text).then(() => {
-        toast.success('Copiado con éxito');
-      });
-    };
-  }
-
+function PromptCard({ img }: PromptCardProps) {
   return (
-    <div className="my-1" onClick={() => handleCopy(props.text3)}>
-      <div className="float-right">
-        <Image
-          src={props.img}
-          alt=""
-          width={200}
-          height={200}
-          className="object-cover rounded-full"
-        />
-      </div>
-      <a
-        href="#"
-        onClick={handleCopy(props.text3)}
-        className="block p-9 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-      >
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          {props.text1}
-        </h5>
-
-        <h6 className="mb-2 text-xl tracking-tight text-gray-900 dark:text-white">
-          {props.text2}
-        </h6>
-        <p className="font-normal text-gray-700 dark:text-gray-400">
-          {props.text3}
-        </p>
-      </a>
+    <div
+      className="my-1"
+      style={{ display: 'inline-block', margin: '10px', width: '200px', height: '200px' }}
+    >
+      <Image src={img} alt="" width={200} height={200} objectFit="cover" />
     </div>
   );
 }
--- app/components/Prompts.tsx
+++ app/components/Prompts.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import PromptCard from './PromptCard';
 
 export default function Prompts() {
@@ -5,54 +6,46 @@ export default function Prompts() {
     {
       img: 'https://photoshot.app/prompts/sacha/viking.png',
       text1: 'Instrucciones para crear un Avatar Vikingo',
-      text2: 'Conviértete en un vikingo con nuestra guía de IA gratuita',
-      text3:
-        'Retrato en primer plano de @me como un vikingo, ultra realista, arte conceptual, detalles intrincados, poderoso y feroz, altamente detallado, hiperrealista, renderizado con Octane, 8K, motor Unreal. Arte de Artgerm, Greg Rutkowski, Charlie Bowater, Magali Villeneuve y Alphonse Mucha, con la hora dorada, cuernos y trenzas en el pelo, capa y casco forrados de piel, hacha en mano, mirando hacia la cámara'
     },
     {
       img: 'https://photoshot.app/prompts/sacha/superhero.png',
       text1: 'Instrucciones para crear un Superhéroe',
-      text2: 'Desata tus poderes con nuestra guía de IA gratuita',
-      text3:
-        'Retrato en primer plano de @me como un superhéroe, increíblemente poderoso, con un traje de alta tecnología y colores vibrantes. Pose heroica, expresión determinada, rodeado de energía y efectos especiales. Arte inspirado en los cómics de Marvel y DC, con estilo de Jim Lee, Alex Ross y Todd McFarlane.'
     },
     {
       img: 'https://photoshot.app/prompts/sacha/astronaut.png',
       text1: 'Instrucciones para crear un Astronauta',
-      text2: 'Conviértete en un astronauta con nuestra guía de IA gratuita',
-      text3:
-        ' Retrato detallado de cerca de @me como astronauta, futurista, altamente detallado, ultra realista, arte conceptual, texturas intrincadas, fondo interestelar, viaje espacial, arte por Alphonse Mucha, Ryan Kittleson, Greg Rutkowski, Leesha Hannigan, Stephan Martiniere, Stanley Artgerm Lau.'
     },
     {
       img: 'https://photoshot.app/prompts/romy/wizard.png',
       text1: 'Instrucciones para crear un Mago',
-      text2: 'Conviértete en Mago con nuestra sugerencia gratuita de IA.',
-      text3:
-        ' Retrato detallado de cerca de @me como un mago, con un concepto de arte fantástico, con detalles e texturas intrincadas, mágico, colorido, arte por wlop, greg rutkowski, charlie bowater, magali villeneuve, alphonse mucha, surrealista, mirando hacia el horizonte, sosteniendo un bastón, con fuego y estrellas en el fondo.'
     },
     {
       img: 'https://photoshot.app/prompts/romy/hobbit.png',
       text1: 'Instrucciones para crear un Hobbit',
-      text2: 'Conviértete en Hobbit con nuestra sugerencia gratuita de IA.',
-      text3:
-        ' Retrato detallado de cerca de @me como un Hobbit, pequeño, con grandes ojos marrones, ropa verde y marrón, rasgos faciales detallados, pies pequeños, cabello ondulado, concepto de arte fantástico, tendencia en ArtStation, altamente detallado, arte de John Howe, Alan Lee y Weta Workshop, colores terrosos, mirando a la cámara.'
     },
     {
       img: 'https://photoshot.app/prompts/sacha/clown.png',
       text1: 'Instrucciones para crear un Payaso',
-      text2: 'Conviértete en Payaso con nuestra sugerencia gratuita de IA.',
-      text3:
-        ' Retrato detallado de cerca de @me como un payaso, altamente detallado, surrealista, rostro inexpresivo, colores brillantes, iluminación contrastante, fondo abstracto, arte de wlop, Greg Rutkowski, Charlie Bowater, Magali Villeneuve, Alphonse Mucha, estilo caricaturesco, inspirado en cómics.'
     }
   ];
+  const chunkSize = 3; // Number of images in each horizontal row
+
+  // Chunk the promptInfo array into smaller arrays of size 'chunkSize'
+  const chunkedPrompts = [];
+  for (let i = 0; i < promptInfo.length; i += chunkSize) {
+    chunkedPrompts.push(promptInfo.slice(i, i + chunkSize));
+  }
+
 
   return (
-    <div>
-      <div className="w-full items-center">
-        {promptInfo.map((prompt) => {
-          return <PromptCard props={prompt} key={Math.random()} />;
-        })}
-      </div>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      {chunkedPrompts.map((chunk, rowIndex) => (
+        <div key={rowIndex} style={{ display: 'flex' }}>
+          {chunk.map((prompt, index) => (
+            <PromptCard img={prompt.img} key={index} />
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
