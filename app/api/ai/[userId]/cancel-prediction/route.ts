16e56b99d672a6508424b6bdfdbbfc6af19b0fb8
j1-dev <aephyxen@gmail.com>
2023-08-20 13:02:58 +0200

    Simplified cancel-training route

--- app/api/ai/[userId]/cancel-prediction/route.ts
+++ app/api/ai/[userId]/cancel-prediction/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from 'next/server';
+import replicate from '@/app/core/clients/replicate';
 
 export async function POST(
   request: Request,
@@ -7,16 +8,8 @@ export async function POST(
   const req = await request.json();
   const predictionId = req.prediction_id as string;
 
-  const apiUrl = `https://api.replicate.com/v1/predictions/${predictionId}/cancel`;
-  const response = await fetch(apiUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      'Accept-Encoding': '*',
-      Authorization: `Token ${process.env.NEXT_PUBLIC_REPLICATE_API_TOKEN}`
-    }
-  });
-  console.log(response.status);
-  return NextResponse.json({ succesful: response.status === 200 });
+  const response = replicate.predictions.cancel(predictionId);
+
+  console.log((await response).error);
+  return NextResponse.json({ succesful: (await response).error === null });
 }
