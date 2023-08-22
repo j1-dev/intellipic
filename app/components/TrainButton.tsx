baebad2ba9c315a52332243523c09ef7933e03b8
j1-dev <aephyxen@gmail.com>
2023-08-22 14:57:22 +0200

    Removed Entrenar from navbar

--- app/components/TrainButton.tsx
+++ app/components/TrainButton.tsx
@@ -15,18 +15,20 @@ export const TrainButton = ({ userData }: { userData: userDataType }) => {
       : 'Compra tokens en la tienda';
 
   return (
-    <button
-      disabled={userData.model_tokens === 0 && userData.dataset === null}
-      className="rounded-lg group dark:shadow-slate-300 hover:shadow-lg border border-black dark:border-white transition-all ease-in-out duration-75 hover:scale-[1.03] disabled:bg-black disabled:text-white dark:disabled:bg-white dark:disabled:text-black"
-      onClick={() => router.push('train/')}
-    >
-      <AiOutlinePlus
-        size={50}
-        className="w-20 m-auto translate-y-2 group-hover:-translate-y-1 transition-all duration-75"
-      />
-      <span className="opacity-0 group-hover:opacity-100 transtition-opacity duration-200">
-        {message}
-      </span>
-    </button>
+    <div className="rounded-lg  dark:shadow-slate-300 hover:shadow-lg border border-black dark:border-white transition-all ease-in-out duration-75 hover:scale-[1.03] hover:bg-black hover:text-white dark:hover:bg-white dark:hover:text-black">
+      <button
+        disabled={userData.model_tokens === 0 && userData.dataset === null}
+        className="w-full h-full py-3"
+        onClick={() => router.push('train/')}
+      >
+        <AiOutlinePlus
+          size={50}
+          className="w-20 m-auto translate-y-2 group-hover:-translate-y-1 transition-all duration-75"
+        />
+        <span className="opacity-0 group-hover:opacity-100 transtition-opacity duration-200">
+          {message}
+        </span>
+      </button>
+    </div>
   );
 };
