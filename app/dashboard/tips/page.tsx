01aeb4664ff4aff3cc21908c31158dfb489d1b02
j1-dev <aephyxen@gmail.com>
2023-08-25 13:29:57 +0200

    Created Consejos page

---
+++ app/dashboard/tips/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function TipsPage() {
+  return (
+    <div className="py-8">
+      <div className="max-w-screen-lg mx-auto px-8">
+        <h2 className="text-4xl font-bold mb-4">Consejos 📖</h2>
+      </div>
+    </div>
+  );
+}
