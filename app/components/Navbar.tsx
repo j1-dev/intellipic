fae243d31f7e544e7bde8d722fe8d38cd5824b60
j1-dev <aephyxen@gmail.com>
2023-08-25 13:30:06 +0200

    Added Consejos page to Navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -64,19 +64,19 @@ export default function Navbar() {
               <span className="absolute bottom-0 left-0 h-0 w-0 border-b border-l border-black dark:border-white opacity-0 group-hover:opacity-100 group-hover:h-[14px] group-hover:w-[14px] transition-all duration-350 ease-in-out transform translate-x-full translate-y-1/2"></span>
             </Link>
           </li>
-          {/* <li
+          <li
             className="list-none lg:mx-2 mx-0 my-2 relative group grow xs:grow-0"
             key={Math.random()}
           >
             <Link
-              href={`/dashboard/${user?.id}/train`}
+              href={`/dashboard/tips`}
               className="font-bold lg:text-2xl md:text-xl xs:text-base py-2 lg:px-7 px-4 text-black dark:text-white no-underline text-xl relative transition-all duration-350 ease-in-out"
             >
-              {windowWidth > 479 ? 'Entrenar' : '🦾'}
+              {windowWidth > 479 ? 'Consejos' : '📖'}
               <span className="absolute top-0 right-0 h-0 w-0 border-t border-r border-black dark:border-white opacity-0 group-hover:opacity-100 group-hover:h-[14px] group-hover:w-[14px] transition-all duration-350 ease-in-out transform -translate-x-full -translate-y-1/2"></span>
               <span className="absolute bottom-0 left-0 h-0 w-0 border-b border-l border-black dark:border-white opacity-0 group-hover:opacity-100 group-hover:h-[14px] group-hover:w-[14px] transition-all duration-350 ease-in-out transform translate-x-full translate-y-1/2"></span>
             </Link>
-          </li> */}
+          </li>
           <li
             className="list-none lg:mx-2 mx-0 my-2 relative group grow xs:grow-0"
             key={Math.random()}
