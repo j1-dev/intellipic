3726222b8b3d495cbec1d9231e0d813eac26b379
j1-dev <aephyxen@gmail.com>
2023-08-25 18:04:01 +0200

    Centered TipsToggle and made it responsive

--- app/components/TipsToggle.tsx
+++ app/components/TipsToggle.tsx
@@ -1,21 +1,34 @@
-'use client';
 import { Switch } from '@headlessui/react';
 import { useState } from 'react';
 
-const TipsToggle = () => {
-  const [enabled, setEnabled] = useState(false);
+interface TipsToggleProps {
+  toggle: boolean;
+  toggleHandler: (newToggleValue: boolean) => void;
+}
+
+const TipsToggle: React.FC<TipsToggleProps> = ({ toggle, toggleHandler }) => {
+  const [enabled, setEnabled] = useState(toggle);
+
+  const handleToggleChange = () => {
+    setEnabled(!enabled);
+    toggleHandler(!enabled);
+  };
 
   return (
     <Switch
-      onChange={() => setEnabled(!enabled)}
-      className="bg-black dark:bg-white transition-all hover:shadow-lg relative inline-flex h-[74px] w-[316px] shrink-0 cursor-pointer rounded-lg border-2 border-transparent duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75"
+      onChange={handleToggleChange}
+      className="bg-black dark:bg-white transition-all hover:shadow-lg relative inline-flex h-[74px] w-[158px] xs:w-[316px] cursor-pointer rounded-lg border-2 border-transparent duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75"
     >
       <div
         aria-hidden="true"
-        className={`${enabled ? 'translate-x-[156px]' : 'translate-x-0'}
-            pointer-events-none inline-block h-[70px] w-[156px] transform rounded-xl bg-white dark:bg-black shadow-lg ring-0 transition duration-200 ease-in-out py-[20px]`}
+        className={`${
+          enabled
+            ? 'xs:translate-x-[156px] translate-x-[77px]'
+            : 'translate-x-0'
+        }
+            pointer-events-none inline-block h-[70px] w-[77px] xs:w-[156px] transform rounded-xl bg-white dark:bg-black shadow-lg ring-0 transition duration-200 ease-in-out py-[24px] xs:py-[20px] `}
       >
-        <span className="font-bold text-xl">
+        <span className="font-bold xs:text-xl text-md">
           {enabled ? 'Entrenar' : 'Generar'}
         </span>
       </div>
