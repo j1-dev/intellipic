c0d560b8a017c033c587f29b588d858c51ad036d
j1-dev <aephyxen@gmail.com>
2023-08-25 18:03:46 +0200

    created and added TipsMain to the page

---
+++ app/components/TipsMain.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TipsMain = ({ toggle }: { toggle: boolean }) => {
+  return (
+    <main className="max-w-screen-lg">
+      <div></div>
+    </main>
+  );
+};
+
+export default TipsMain;
--- app/dashboard/tips/page.tsx
+++ app/dashboard/tips/page.tsx
@@ -1,16 +1,22 @@
-import Image from 'next/image';
-import Link from 'next/link';
+'use client';
+import React, { useState } from 'react';
 import TipsToggle from '@/app/components/TipsToggle';
+import TipsMain from '@/app/components/TipsMain';
 
-export default function TipsPage() {
+const TipsPage: React.FC = () => {
+  const [toggle, setToggle] = useState(true);
+
   return (
     <div className="py-8">
       <div className="max-w-screen-lg mx-auto px-8">
         <h2 className="text-4xl font-bold mb-4">Consejos 📖</h2>
-        <main>
-          <TipsToggle />
+        <main className="flex justify-center items-center">
+          <TipsToggle toggle={toggle} toggleHandler={setToggle} />
+          <TipsMain toggle={toggle} />
         </main>
       </div>
     </div>
   );
-}
+};
+
+export default TipsPage;
