e0121567b489399fc4730da53af81e4110573c08
j1-dev <aephyxen@gmail.com>
2023-09-17 20:42:58 +0200

    Debugging 3

--- app/[locale]/tos/page.tsx
+++ app/[locale]/tos/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { useTranslations } from 'next-intl';
 
 const Terms = () => {
--- components/FaqMain.tsx
+++ components/FaqMain.tsx
@@ -1,4 +1,3 @@
-'use-client';
 import { useTranslations } from 'next-intl';
 
 const FaqMain = () => {
