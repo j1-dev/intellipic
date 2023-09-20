ef83a8da3f6cb8a416aa5bdee9bddcc042c92676
j1-dev <aephyxen@gmail.com>
2023-09-20 20:20:52 +0200

    Hotfix

--- components/LangToggle.tsx
+++ components/LangToggle.tsx
@@ -8,26 +8,17 @@ const LangToggle = () => {
   const pathname = usePathname();
   const router = useRouter();
   const locale = useLocale();
-  const [enabled, setEnabled] = useState<boolean>(
-    locale === 'es' ? true : false
-  );
-
-  function toggleLang() {
-    setEnabled(!enabled);
-    const l = locale === 'es' ? 'en' : 'es';
-    router.replace(pathname, { locale: l });
-  }
 
   return (
-    <Menu as="div">
+    <Menu as="div" className="inline-block">
       <Menu.Button className="inline-flex w-full justify-center rounded-md  px-4 py-2 text-sm font-medium card focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
         Idoma
       </Menu.Button>
-      <Menu.Items className="absolute top-0 lef-0 block -translate-y-9  -translate-x-8 border bg-white dark:bg-black border-black dark:border-white px-3 rounded-md">
+      <Menu.Items className="absolute top-0 lef-0 inline-block -translate-y-20  -translate-x-[84px] border bg-white dark:bg-black border-black dark:border-white px-2 rounded-md">
         <Menu.Item>
           {({ active }) => (
             <button
-              className="bg-[url('https://cdn.ipregistry.co/flags/twemoji/es.png')] bg-contain w-10 h-10 hover:scale-105 transition-all m-2"
+              className="bg-[url('https://cdn.ipregistry.co/flags/twemoji/es.png')] bg-contain w-10 h-10 hover:scale-105 transition-all mt-2"
               onClick={() => {
                 router.replace(pathname, { locale: 'es' });
               }}
@@ -39,7 +30,7 @@ const LangToggle = () => {
         <Menu.Item>
           {({ active }) => (
             <button
-              className="bg-[url('https://cdn.ipregistry.co/flags/twemoji/gb.png')] bg-contain w-10 h-10 hover:scale-105 transition-all m-2"
+              className="bg-[url('https://cdn.ipregistry.co/flags/twemoji/gb.png')] bg-contain w-10 h-10 hover:scale-105 transition-all mb-2"
               onClick={() => {
                 router.replace(pathname, { locale: 'en' });
               }}
