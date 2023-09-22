b5cc7733353cc8bc8bb1f360e18bfcfca00df54f
j1-dev <aephyxen@gmail.com>
2023-09-22 10:00:35 +0200

    Fixed LangToggle

--- components/LangToggle.tsx
+++ components/LangToggle.tsx
@@ -13,7 +13,7 @@ const LangToggle = () => {
       <Menu.Button className="inline-flex w-full justify-center rounded-md  px-4 py-2 text-sm font-medium card focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
         {locale === 'es' ? 'Idioma' : 'Language'}
       </Menu.Button>
-      <Menu.Items className="absolute top-0 lef-0 inline-block -translate-y-20  -translate-x-[84px] border bg-white dark:bg-black border-black dark:border-white px-2 rounded-md">
+      <Menu.Items className="-top-2 inset-y-0 left-1/2 transform -translate-x-1/2 -translate-y-full  absolute lef-0 inline-block border bg-white dark:bg-black border-black dark:border-white px-2 rounded-md h-[106px]">
         <Menu.Item>
           {({ active }) => (
             <button
