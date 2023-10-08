7bdf9b1b08197670f26955bdc764beb1d38965b9
David Sedeno <davidsed9@gmail.com>
2023-10-08 14:16:01 +0100

    dashboardphotoupdate

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -99,7 +99,7 @@ export default function Home() {
               <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
                 <Image
                   alt=""
-                  src="/Images/Elon.png"
+                  src="/Images/Mask.png"
                   layout="fill"
                   objectFit="cover"
                   className="group-hover:opacity-75  transition-all"
@@ -111,7 +111,7 @@ export default function Home() {
               <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
                 <Image
                   alt=""
-                  src="/Images/Messi3.png"
+                  src="/Images/f1driver.png"
                   layout="fill"
                   objectFit="cover"
                   className="group-hover:opacity-75 transition-all"
