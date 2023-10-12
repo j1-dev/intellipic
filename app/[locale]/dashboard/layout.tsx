399e0a0b5a42b0b43f099ad89584489268c740e2
j1-dev <aephyxen@gmail.com>
2023-10-12 20:45:23 +0200

    Added metadata to improve SEO

--- app/[locale]/dashboard/layout.tsx
+++ app/[locale]/dashboard/layout.tsx
@@ -1,7 +1,40 @@
 import Navbar from '@/components/Navbar';
 
 export const metadata = {
-  title: 'IntelliPic'
+  title: 'IntelliPic',
+  description:
+    'Intellipic es una aplicación de Inteligencia Artificial (IA) Generativa capaz de crear imágenes de la nada con cualquier concepto que tu le quieras enseñar',
+  generator: 'IntelliPic',
+  applicationName: 'IntelliPic',
+  referrer: 'origin-when-cross-origin',
+  keywords: [
+    'IntelliPic',
+    'Stable Diffusion',
+    'SDXL',
+    'Inteligencia Artificial',
+    'AI',
+    'IA'
+  ],
+  authors: [{ name: 'Juan García' }, { name: 'David Sedeño' }],
+  colorScheme: 'dark',
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1
+    }
+  }
 };
 
 export default async function RootLayout({
