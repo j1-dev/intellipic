bdebada1dd17eb950b3573bc5ff3554921b27e3e
j1-dev <aephyxen@gmail.com>
2023-11-02 12:00:55 +0100

    Added payment status check to webhook route

--- app/api/shop/webhook/route.ts
+++ app/api/shop/webhook/route.ts
@@ -38,44 +38,47 @@ export async function POST(request: Request) {
       const paymentIntent = event.data.object as Stripe.Checkout.Session;
       const userId = paymentIntent?.metadata?.userId;
       const amount = paymentIntent?.amount_total;
+      const status = paymentIntent?.payment_status;
 
-      const { data: userData } = await supabase
-        .from('user-data')
-        .select('*')
-        .eq('id', userId);
+      if (status === 'paid') {
+        const { data: userData } = await supabase
+          .from('user-data')
+          .select('*')
+          .eq('id', userId);
 
-      let tokenAmountGenerating = 0;
-      let tokenAmountTraining = 0;
+        let tokenAmountGenerating = 0;
+        let tokenAmountTraining = 0;
 
-      switch (amount) {
-        case 100:
-          tokenAmountGenerating = 10;
-          break;
-        case 200:
-          tokenAmountGenerating = 25;
-          break;
-        case 300:
-          tokenAmountGenerating = 40;
-          break;
-        case 400:
-          tokenAmountGenerating = 20;
-          tokenAmountTraining = 1;
-          break;
-        case 700:
-          tokenAmountGenerating = 100;
-          break;
-        default:
-          break;
+        switch (amount) {
+          case 100:
+            tokenAmountGenerating = 10;
+            break;
+          case 200:
+            tokenAmountGenerating = 25;
+            break;
+          case 300:
+            tokenAmountGenerating = 40;
+            break;
+          case 400:
+            tokenAmountGenerating = 20;
+            tokenAmountTraining = 1;
+            break;
+          case 700:
+            tokenAmountGenerating = 100;
+            break;
+          default:
+            break;
+        }
+
+        const { data, error } = await supabase
+          .from('user-data')
+          .update({
+            model_tokens: userData?.[0].model_tokens + tokenAmountTraining,
+            image_tokens: userData?.[0].image_tokens + tokenAmountGenerating,
+            last_payment_status: 'paid'
+          })
+          .eq('id', userId);
       }
-
-      const { data, error } = await supabase
-        .from('user-data')
-        .update({
-          model_tokens: userData?.[0].model_tokens + tokenAmountTraining,
-          image_tokens: userData?.[0].image_tokens + tokenAmountGenerating,
-          last_payment_status: 'paid'
-        })
-        .eq('id', userId);
 
       break;
     // ... handle other event types
