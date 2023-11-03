86184092d8cedbac9b6bcc3cdb30c2f9e2c34b3d
David Sedeno <davidsed9@gmail.com>
2023-11-03 14:00:31 +0000

    Model Card redesigned

--- components/ModelCard.tsx
+++ components/ModelCard.tsx
@@ -34,21 +34,20 @@ export default function ModelCard({
   }
 
   return (
-    <div className="card group">
-      <Link
-        href={`/dashboard/${user}/${model}`}
-        className=" w-full h-full py-10"
-      >
-        <button className="w-full h-full">
-          <div className="w-full h-full block">
-            <p className="font-bold text-xl m-5">
+    
+      <div className="bg-white  rounded-md p-4 border border-black hover:shadow-md transition-transform transform hover:scale-105">
+      <Link href={`/dashboard/${user}/${model}`} passHref>
+        <div className="block w-full h-full cursor-pointer">
+          <div className="w-full h-full flex flex-col justify-center items-center">
+            <p className="font-bold text-xl mb-3 text-gray-800">
               {t('modelName') + props.token}
             </p>
-            <div className="h-0 border-b-[1px] border-black group-hover:border-white dark:group-hover:border-black dark:border-white transition-all" />
-            <p className="m-1">{t('modelStatus') + translateStatus(status)}</p>
+            <div className="w-16 border-b-2 border-blue-500 mb-3 "></div>
+            <p className="text-sm text-gray-600">{t('modelStatus') + translateStatus(status)}</p>
           </div>
-        </button>
+        </div>
       </Link>
     </div>
-  );
+  
+);
 }
