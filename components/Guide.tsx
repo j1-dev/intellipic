fccecccba82dabe06f0edb59da67eb3aa25abe5b
David Sedeno <davidsed9@gmail.com>
2023-11-16 14:04:15 +0000

    style fondation

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -14,88 +14,92 @@ import image11 from '@/public/Images/Guide/image11.png';
 import image12 from '@/public/Images/Guide/image12.png';
 
 const Guide = () => {
-    const t = useTranslations('guide');
+  const t = useTranslations('guide');
 
-    return (
-    <div>
+  return (
+    <div className="max-w-screen-lg mx-auto px-4">
+      <div className="mt-8">
         <h2 className="text-4xl font-bold mb-4">{t('trainingModel')}</h2>
-            <div className="max-w-screen-lg mt-5">
-                <ul>
-                    <li>
-                        <h3 className="text-xl font-bold">{t('trainingStep1')}</h3>
-                        <Image alt="" src={image1} className="group-hover:opacity-75  transition-all"/>
-                    </li>
-                    <li>
-                        <h3 className="text-xl font-bold">{t('trainingStep2')}</h3>
-                        <Image alt="" src={image2} className="group-hover:opacity-75  transition-all"/>
-                    </li>
-                    <li>
-                        <h3 className="text-xl font-bold">{t('trainingStep3')}</h3>
-                        <div className='flex'>
-                        <Image alt="" src={image3} className="group-hover:opacity-75  transition-all"/>
-                        <Image alt="" src={image4} className="group-hover:opacity-75  transition-all"/>
-                        </div>
-                    </li>
-                </ul>
-          
-        </div>
+        <ul className="space-y-6">
+          <li className="flex items-center">
+            <h3 className="text-xl font-bold">{t('trainingStep1')}</h3>
+            <Image alt="" src={image1} className="ml-4 group-hover:opacity-75 transition-all" />
+          </li>
+          <li className="flex items-center">
+            <h3 className="text-xl font-bold">{t('trainingStep2')}</h3>
+            <Image alt="" src={image2} className="ml-4 group-hover:opacity-75 transition-all" />
+          </li>
+          <li className="flex items-center">
+            <h3 className="text-xl font-bold">{t('trainingStep3')}</h3>
+            <div className="flex ml-4 space-x-2">
+              <Image alt="" src={image3} className="w-40 h-auto group-hover:opacity-75 transition-all" />
+              <Image alt="" src={image4} className="group-hover:opacity-75 transition-all" />
+            </div>
+          </li>
+        </ul>
+      </div>
+
+      <div className="mt-8">
         <h2 className="text-4xl font-bold mb-4">{t('generatingImages')}</h2>
-            <div className="max-w-screen-lg mt-5">
-                <ul>
-                    <li>
-                        <h3 className="text-xl font-bold">{t('generatingImageStep1')}</h3>
-                        <Image alt="" src={image5} className="group-hover:opacity-75  transition-all"/>
-                    </li>
-                    <li>
-                        <h3 className="text-xl font-bold">{t('generatingImageStep2')}</h3>
-                    </li>
-                    <li>
-                        <h3 className="text-xl font-bold">{t('generatingImageStep3')}</h3>
-                    </li>
-                </ul>
-            </div>
+        <ul className="space-y-6">
+          <li className="items-center">
+            <h3 className="text-xl font-bold py-4 px-4">{t('generatingImageStep1')}</h3>
+            <Image alt="" src={image5} className="ml-4 group-hover:opacity-75 transition-all" />
+          </li>
+          <li className="flex items-center">
+            <h3 className="text-xl font-bold">{t('generatingImageStep2')}</h3>
+          </li>
+          <li className="flex items-center">
+            <h3 className="text-xl font-bold">{t('generatingImageStep3')}</h3>
+          </li>
+        </ul>
+      </div>
+
+      <div className="mt-8">
         <h2 className="text-4xl font-bold mb-4">{t('PredefGuideEx')}</h2>
-            <div className="max-w-screen-lg mt-5">
-                <ul>
-                    <li>
-                        <h3 className="text-xl font-bold">{t('PredefGuideStep1')}</h3>
-                        <Image alt="" src={image6} className="group-hover:opacity-75  transition-all"/>
-                    </li>
-                    <li>
-                        <h3 className="text-xl font-bold">{t('PredefGuideStep2')}</h3>
-                        <Image alt="" src={image7} className="group-hover:opacity-75  transition-all"/>
-                    </li>
-                    <li>
-                        <h3 className="text-xl font-bold">{t('PredefGuideStep3')}</h3>
-                        <Image alt="" src={image8} className="group-hover:opacity-75  transition-all"/>
-                    </li>
-                </ul>
-            </div>
-            
-            <h3 className="text-4xl font-bold mb-4">{t('generatingImageStep3')}</h3>
-             <Image alt="" src={image9} className="group-hover:opacity-75  transition-all"/>
-                <div className="max-w-screen-lg mt-5">
-                <h3 className="text-4xl font-bold mb-4">{t('ManualGuide')}</h3>
-                    <ul>
-                        <li>
-                            <h3 className="text-xl font-bold">{t('ManualStep0')}</h3>
-                        </li>
-                        <li>
-                            <h3 className="text-xl font-bold">{t('ManualStep1')}</h3>
-                            <Image alt="" src={image10} className="group-hover:opacity-75  transition-all"/>
-                        </li>
-                        <li>
-                            <h3 className="text-xl font-bold">{t('ManualStep2')}</h3>
-                            <Image alt="" src={image11} className="group-hover:opacity-75  transition-all"/>
-                        </li>
-                        <li>
-                            <h3 className="text-xl font-bold">{t('ManualStep3')}</h3>
-                            <Image alt="" src={image12} className="group-hover:opacity-75  transition-all"/>
-                        </li>
-                    </ul>
-                </div>
-        </div>
-        );
+        <ul className="space-y-6">
+          <li className="flex items-center">
+            <h3 className="text-xl font-bold">{t('PredefGuideStep1')}</h3>
+            <Image alt="" src={image6} className="ml-4 group-hover:opacity-75 transition-all" />
+          </li>
+          <li className="flex items-center">
+            <h3 className="text-xl font-bold">{t('PredefGuideStep2')}</h3>
+            <Image alt="" src={image7} className="ml-4 group-hover:opacity-75 transition-all" />
+          </li>
+          <li className="flex items-center">
+            <h3 className="text-xl font-bold">{t('PredefGuideStep3')}</h3>
+            <Image alt="" src={image8} className="ml-4 group-hover:opacity-75 transition-all" />
+          </li>
+        </ul>
+      </div>
+
+      <div className="mt-8">
+        <h2 className="text-xl font-bold mb-4">{t('generatingImageStep3')}</h2>
+        <Image alt="" src={image9} className="group-hover:opacity-75 transition-all" />
+        <div className="max-w-screen-lg mt-5">
+          <h3 className="text-4xl font-bold mb-4">{t('ManualGuide')}</h3>
+          <ul className="space-y-6">
+            <li className="flex items-center">
+              <h3 className="text-xl font-bold">{t('ManualStep0')}</h3>
+          </li>
+          <li className="flex items-center">
+            <h3 className="text-xl font-bold">{t('ManualStep1')}</h3>
+            <Image alt="" src={image10} className="ml-4 group-hover:opacity-75 transition-all" />
+          </li>
+          <li className="flex items-center">
+            <h3 className="text-xl font-bold">{t('ManualStep2')}</h3>
+            <Image alt="" src={image11} className="ml-4 group-hover:opacity-75 transition-all" />
+          </li>
+          <li className="flex items-center">
+            <h3 className="text-xl font-bold">{t('ManualStep3')}</h3>
+            <Image alt="" src={image12} className="ml-4 group-hover:opacity-75 transition-all" />
+          </li>
+        </ul>
+      </div>
+      </div>
+    </div>
+  );
+  
 };
 
 export default Guide;
