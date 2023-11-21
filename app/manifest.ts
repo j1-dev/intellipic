57cb0e18164bb77e31c97f746b8b7de090d85501
j1-dev <aephyxen@gmail.com>
2023-11-21 19:57:32 +0100

    testing 1

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,55 +2,62 @@ import { MetadataRoute } from 'next';
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Intellipic',
-    short_name: 'Intellipic',
+    name: 'IntelliPic',
+    short_name: 'IntelliPic',
     theme_color: '#000000',
     background_color: '#000000',
     description:
       'Intellipic es una aplicación de Inteligencia Artificial (IA) Generativa capaz de crear imágenes de la nada con cualquier concepto que tu le quieras enseñar',
     start_url: '/',
-    scope: '/',
     display: 'standalone',
     icons: [
       {
         src: '/favicon.ico',
         sizes: 'any',
-        type: 'image/x-icon'
+        type: 'image/x-icon',
+        purpose: 'any'
       },
       {
         src: 'public/icons/maskable_icon_x48.png',
         sizes: '48x48',
-        type: 'image/png'
+        type: 'image/png',
+        purpose: 'any'
       },
       {
         src: 'public/icons/maskable_icon_x72.png',
         sizes: '72x72',
-        type: 'image/png'
+        type: 'image/png',
+        purpose: 'any'
       },
       {
         src: 'public/icons/maskable_icon_x96.png',
         sizes: '96x96',
-        type: 'image/png'
+        type: 'image/png',
+        purpose: 'any'
       },
       {
         src: 'public/icons/maskable_icon_x128.png',
         sizes: '128x128',
-        type: 'image/png'
+        type: 'image/png',
+        purpose: 'any'
       },
       {
         src: 'public/icons/maskable_icon_x192.png',
         sizes: '192x192',
-        type: 'image/png'
+        type: 'image/png',
+        purpose: 'any'
       },
       {
         src: 'public/icons/maskable_icon_x384.png',
         sizes: '384x384',
-        type: 'image/png'
+        type: 'image/png',
+        purpose: 'any'
       },
       {
         src: 'public/icons/maskable_icon_x512.png',
         sizes: '512x512',
-        type: 'image/png'
+        type: 'image/png',
+        purpose: 'any'
       },
       {
         src: 'public/icons/maskable_icon_x192.png',
