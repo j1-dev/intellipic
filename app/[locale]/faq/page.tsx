599765d1d8384e551f8d3ba1b4011e63d6c5c7d0
j1-dev <aephyxen@gmail.com>
2023-12-26 10:42:42 +0100

    Fixed padding on faq page

--- app/[locale]/faq/page.tsx
+++ app/[locale]/faq/page.tsx
@@ -7,7 +7,7 @@ import { BsArrowReturnLeft } from 'react-icons/bs';
 const Faq = () => {
   const router = useRouter();
   return (
-    <div className="mx-auto max-w-screen-lg px-10 rounded-lg w-full bg-white text-black dark:bg-black dark:text-white transition-all my-4">
+    <div className="mx-auto max-w-screen-lg p-6 rounded-lg w-full bg-white text-black dark:bg-black dark:text-white transition-all my-4">
       <header className="py-8">
         <nav className="max-w-screen-lg mx-auto flex justify-between items-center">
           <button onClick={router.back} className="inline-flex">
